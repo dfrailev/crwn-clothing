98fafb1c5390a7b4d004d45c6a4d22bc69b3c6e0
dfraile <dfraile@tecnogram.net>
2019-08-01 13:31:54 +0200

    Lesson 70: header. Also Directory now working and homepage back to work

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import './App.css';
 
 import HomePage from  './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+/* Does it work with or wihtout .jsx? */
 
 function App() {
   /* Switch shows just one of options, the first matches */
-  return <div>
-    <Switch>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/shop' component={ShopPage} />
-    </Switch>
-  </div>;
+  return (
+    <div>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
