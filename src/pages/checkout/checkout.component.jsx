fdc4df186b62d794a93d13017885f0a6f5e3b793
dfraile <dfraile@tecnogram.net>
2019-09-02 16:49:16 +0200

    Clear item from basket

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -21,6 +21,9 @@ const CheckoutPage = ({cartItems, total}) => (
                 <span>Quantity</span>
             </div>
             <div className='header-block'>
+                <span>Price</span>
+            </div>
+            <div className='header-block'>
                 <span>Remove</span>
             </div>
         </div> 
