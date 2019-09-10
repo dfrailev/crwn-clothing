2b1359bfbce1e9dee31911677d5715528d1b13e0
dfraile <dfraile@tecnogram.net>
2019-09-10 17:58:59 +0200

    Directory and shop data into its own reducer, created selector, updated components

--- src/redux/shop/shop.data.js
+++ src/redux/shop/shop.data.js
@@ -1,5 +1,5 @@
-const SHOP_DATA = [
-    {
+const SHOP_DATA = {
+  hats: {
       id: 1,
       title: 'Hats',
       routeName: 'hats',
@@ -57,10 +57,10 @@ const SHOP_DATA = [
           name: 'Blue Snapback',
           imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
           price: 16
-        }
+        }      
       ]
     },
-    {
+    sneakers: {
       id: 2,
       title: 'Sneakers',
       routeName: 'sneakers',
@@ -115,7 +115,7 @@ const SHOP_DATA = [
         }
       ]
     },
-    {
+    jackets: {
       id: 3,
       title: 'Jackets',
       routeName: 'jackets',
@@ -152,7 +152,7 @@ const SHOP_DATA = [
         }
       ]
     },
-    {
+    womens: {
       id: 4,
       title: 'Womens',
       routeName: 'womens',
@@ -201,7 +201,7 @@ const SHOP_DATA = [
         }
       ]
     },
-    {
+    mens: {
       id: 5,
       title: 'Mens',
       routeName: 'mens',
@@ -244,7 +244,7 @@ const SHOP_DATA = [
         }
       ]
     }
-  ];
+  };
   
   export default SHOP_DATA;
   
