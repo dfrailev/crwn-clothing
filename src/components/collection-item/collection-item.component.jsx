f0b93a848a09a70cb0f6eb487cc27e58d21b03b1
dfraile <dfraile@tecnogram.net>
2019-09-18 18:04:49 +0200

    Collection-item still uses scss so needs styles for collection-item.styles.scss. Adjusted

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -20,9 +20,11 @@ const CollectionItem = ({ item, addItem }) => {
                 <span className='name'>{name}</span>
                 <span className='price'>€{price}</span>
             </div>
-            <CustomButton onClick={()=> addItem(item)} inverted>
-                Add to cart
-            </CustomButton>
+            <div className='custom-button'>
+                <CustomButton onClick={()=> addItem(item)} inverted>
+                    Add to cart
+                </CustomButton>
+            </div>            
         </div>
     
     );
