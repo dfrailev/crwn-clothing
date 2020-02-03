16e90ceb2823cacaf0b8294ad1f64a30df74941c
dfraile <dfraile@tecnogram.net>
2020-02-03 12:38:48 +0100

    Section 18

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,31 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (CollectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: CollectionsMap
-}); 
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+
+})
+
+export const fetchCollectionFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+});
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => { //can dispatch function when redux-thunk is enabled
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap));
+       }).catch(error => dispatch(fetchCollectionFailure(error.message)));
+
+    }
+}
